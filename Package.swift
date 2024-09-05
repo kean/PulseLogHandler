9105188152ebf3bd151270725ab81022e159595a
kean <grebenyuk.alexander@gmail.com>
2024-09-04 22:42:26 -0400

    Update

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let package = Package(
     name: "PulseLogHandler",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
         .tvOS(.v15),
         .macOS(.v12),
         .watchOS(.v8)
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.2"),
-        .package(url: "https://github.com/kean/pulse.git", from: "4.0.0")
+        .package(url: "https://github.com/kean/pulse.git", from: "5.0.0")
     ],
     targets: [
         .target(name: "PulseLogHandler", dependencies: [
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PulseLogHandler
-SwiftLog Extension for Pulse
+
+[SwiftLog](https://github.com/apple/swift-log/blob/main/Package.swift) extensions for [Pulse](https://github.com/kean/Pulse).
+
+## Minimum Requirements
+
+*Matches Pulse*.
+
+## License
+
+Pulse is available under the MIT license. See the LICENSE file for more info.
